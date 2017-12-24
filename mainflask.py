48620414d919f822cf861e6efaac69bf190897e2
ceejimus <cjtheairelement@gmail.com>
2017-12-24 13:10:34 -0600

    found some weird directory javascript drop code

--- mainflask.py
+++ mainflask.py
@@ -183,6 +183,7 @@ def allowed_file(filename):
     
 @application.route('/uploader', methods = ['GET', 'POST'])
 def upload_filer():
+    print("debug")
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
--- static/js/custom/javascript_file_upload.js
+++ static/js/custom/javascript_file_upload.js
@@ -9,7 +9,6 @@ var FileUpload = (function() {
     };
 
     var startUpload = function(files) {
-        console.log(files)
         var formData = new FormData();
         formData.append("file", files[0]);
         var promise = $.ajax({
@@ -47,7 +46,25 @@ var FileUpload = (function() {
                     console.log(data);
             }
         });
-    }
+    };
+
+    var traverseFileTree = function(item, path) {
+      path = path || "";
+      if (item.isFile) {
+        // Get file
+        item.file(function(file) {
+          console.log("File:", path + file.name);
+        });
+      } else if (item.isDirectory) {
+        // Get folder contents
+        var dirReader = item.createReader();
+        dirReader.readEntries(function(entries) {
+          for (var i=0; i<entries.length; i++) {
+            traverseFileTree(entries[i], path + item.name + "/");
+          }
+        });
+      }
+    };
 
     var bindEvents = function(elements) {
         elements.UploadForm.on('submit', function(e) {
@@ -61,7 +78,15 @@ var FileUpload = (function() {
             e.preventDefault();
             $(this).addClass('upload-drop-zone');
 
-            startUpload(e.originalEvent.dataTransfer.files);
+            // startUpload(e.originalEvent.dataTransfer.files);
+            var items = e.originalEvent.dataTransfer.items;
+              for (var i=0; i<items.length; i++) {
+                // webkitGetAsEntry is where the magic happens
+                var item = items[i].webkitGetAsEntry();
+                if (item) {
+                  traverseFileTree(item);
+                }
+              }
         }).on('dragover', function(e) {
             $(this).addClass('drop');
             return false;
